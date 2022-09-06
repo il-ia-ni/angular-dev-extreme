019c3f248fa0eb3711a64c33c9b635985991d763
Ilia Nikolaenko <niil02@sms-group.com>
2022-09-06 13:06:40 +0200

    Updated app component styling, added line comments

--- hello_angular/src/app/app.component.ts
+++ hello_angular/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'hello_angular';
+  title = 'Hello World! by Ilia Nikolaenko';
 }
--- hello_angular/src/app/app.module.ts
+++ hello_angular/src/app/app.module.ts
@@ -5,14 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
+  declarations: [  // Declarables are components, directives and pipes
     AppComponent
   ],
-  imports: [
+  imports: [  // other NgModules that this particular module needs to function properly
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [],  // list of the services the app needs
+  bootstrap: [AppComponent]  // entry components of the app, the app is started with it
 })
 export class AppModule { }
