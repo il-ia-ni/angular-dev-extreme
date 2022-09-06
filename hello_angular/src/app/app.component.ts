bc44da48fea24d0d80a12489f0a1e4156d9f5c72
Ilia Nikolaenko <niil02@sms-group.com>
2022-09-06 13:17:56 +0200

    Prepared the app structure

--- hello_angular/src/app/app.component.ts
+++ hello_angular/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Hello World! by Ilia Nikolaenko';
+  title = 'Menu of the Day';
 }
