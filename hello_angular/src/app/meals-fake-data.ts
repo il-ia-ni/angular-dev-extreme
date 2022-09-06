7fcb2fae6f7816590d53291575f3b6121163eeb7
Ilia Nikolaenko <niil02@sms-group.com>
2022-09-06 14:23:21 +0200

    Added a list of meals, updated template and added event handler and conditional styling

---
+++ hello_angular/src/app/meals-fake-data.ts
@@ -0,0 +1,14 @@
+import { Meal } from './meal';
+
+export const MEALS: Meal[] = [
+  { id: 11, name: 'Pizza Napolitana' },
+  { id: 12, name: 'Pasta Frutti di Mare' },
+  { id: 13, name: 'Bombasto Salatti' },
+  { id: 14, name: 'Celeritas Suppe' },
+  { id: 15, name: 'Magneta Eis' },
+  { id: 16, name: 'Fritties & Fisch' },
+  { id: 17, name: 'Curry Chickenfingers' },
+  { id: 18, name: 'Sellerie Chips' },
+  { id: 19, name: 'Möhrensuppe' },
+  { id: 20, name: 'Apfelsinneis' }
+];
--- hello_angular/src/app/meals/meals.component.ts
+++ hello_angular/src/app/meals/meals.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Meal } from '../meal'
+import { Meal } from '../meal';
+import { MEALS } from '../meals-fake-data'
 
 @Component({  // decorator function that specifies the Angular metadata for the component
   selector: 'app-meals',  // the component's CSS element selector, matches the name of the HTML element that identifies this component within a parent component's html-template (see app.component.html)
@@ -8,15 +9,18 @@ import { Meal } from '../meal'
 })
 export class MealsComponent implements OnInit {  // always declare a comp cls with EXPORT to be able to use it in the App module
 
+  meals = MEALS;
+  selectedMeal!: Meal;
   constructor() { }
 
   ngOnInit(): void {
     /* A lifecycle hook Angular calls ngOnInit shortly after creating a component. It's a good place to put initialization logic. See @ https://v5.angular.io/guide/lifecycle-hooks#oninit */
   }
 
-  meal: Meal = {
-    id: 1,
-    name: "Mac'n'cheese"
-  };
+  onSelect(meal: Meal): void {
+    this.selectedMeal = meal;
+  }
+
+
 
 }
