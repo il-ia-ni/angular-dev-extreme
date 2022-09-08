afa42f733fb371711f9a57352e5f5b8449965274
Ilia Nikolaenko <niil02@sms-group.com>
2022-09-08 17:11:16 +0200

    Updated grid data, added col items and Colmn chooser

--- hello-ng-dev-extreme/src/app/meals.service.ts
+++ hello-ng-dev-extreme/src/app/meals.service.ts
@@ -5,12 +5,16 @@ export class Meal {
   Name!: String
   Description!: String
   Price!: Number
+  Ingredients!: String
+  Energy!: String
+  Vegan!: boolean
+
 }
 
 const meals: Meal[] = [
-  { ID: 1, Name: 'Salat', Description: 'Mit Öl oder Joghurt', Price: 10.50 },
-  { ID: 2, Name: 'Schnitzel', Description: 'Mit pommes', Price: 15.85 },
-  { ID: 3, Name: 'Capuccino', Description: '', Price: 4.50 }
+  { ID: 1, Name: 'Salat', Description: 'Mit Öl oder Joghurt', Price: 10.50, Ingredients: "Zuchhini, Tomaten, Gurke, Basilikum", Energy: "A", Vegan: true },
+  { ID: 2, Name: 'Schnitzel', Description: 'Mit pommes', Price: 15.85, Ingredients: "Rindfleisch, Kräuter, Knoblauch", Energy: "D", Vegan: false },
+  { ID: 3, Name: 'Capuccino', Description: '', Price: 4.50, Ingredients: "Columbianische Bohnen", Energy: "A", Vegan: true }
 ];
 
 @Injectable({
